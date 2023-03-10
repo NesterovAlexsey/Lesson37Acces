0aa4dec88edda03b1e9b69a2fed3e730cabca4a9
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-03-10 07:39:12 +0100

    add method checkPawnStep & setCoordinates

--- src/Task0Pawn/Pawn.java
+++ src/Task0Pawn/Pawn.java
@@ -19,41 +19,42 @@ public class Pawn {
     this.column = column;
   }
 
-  // геттер для номера строки
-  // public - потому что геттер
-  // не static - потому что для конкретной фигуры
   public int getRow() {
     return row;
   }
 
-  // геттер для номера столбца
   public int getColumn() {
     return column;
   }
 
-  // сеттеров отдельно для строки и столбца не будет - можно только менять координаты вместе
-  // "сеттер" для координат
-  // public, потому что сеттер
-  // не static, потому что для конкретной фигуры
-  public void setCoordinates(int row, int column) {
+  public void setCoordinates(int row, int column, Color color) {
     checkCoordinates(row, column);
+    checkPawnStep(row, column, color);
+    this.row = row;
+    this.column = column;
+    checkCoordinates(row, column);
+
     // TODO проверка возможности хода
   }
 
-  // проверка корректности координат
-  // private - потому что "служебный"
-  // static - не связан с конкретной фигурой, предварительная проверка
+  private void checkPawnStep(int row, int column, Color color) {
+    if (Math.abs(row - getRow()) > 1) {
+      throw new IllegalArgumentException("Некорректный новый ход для пешки: " + row);
+    }
+    if (Math.abs(column - getColumn()) > 1) {
+      throw new IllegalArgumentException("Некорректный новый ход для пешки: " + column);
+    }
+    if ((color == Color.WHITE) && (getColumn() > column)) {
+      throw new IllegalArgumentException("Некорректный новый ход для пешки: " + color + column);
+    }
+    if ((color == Color.BLACK) && (getColumn() < column)) {
+      throw new IllegalArgumentException("Некорректный новый ход для пешки: " + color + column);
+    }
+  }
+
   private static void checkCoordinates(int row, int column) {
     if (row < 1 || row > 8) {
       throw new IllegalArgumentException("Некорректный номер строки: " + row);
-      // ошибка - неправильный аргумент
-      // выбрасываем её самостоятельно - теперь наш собственный код может сообщать об ошибке
-      // throw (объект класса Исключение);
-      // throw new КлассИсключения();
-      // КлассИсключения() - конструктор по умолчанию, "просто ошибка"
-      // КлассИсключения(String сообщениеОбОшибке) - ошибка "с подробностями",
-      //  их видно через getMessage()
-      // условие-стражник для метода, в котором нельзя просто написать return
     }
 
     if (column < 1 || column > 8) {
