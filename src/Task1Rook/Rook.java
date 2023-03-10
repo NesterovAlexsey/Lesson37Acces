b7b4a47370f63232c2b3d95d87a73a4a01383397
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-03-10 16:54:02 +0100

    add attributes, constructor, checkCoordinates and get methods

--- src/Task1Rook/Rook.java
+++ src/Task1Rook/Rook.java
@@ -4,4 +4,40 @@
 package Task1Rook;
 
 public class Rook {
+  public enum Color {
+    WHITE,
+    BLACK,
+  }
+
+  private Color color;
+
+  private int row;
+
+  private int column;
+
+  public Rook(Color color, int row, int column) {
+    this.color = color;
+
+    checkCoordinates(row, column);
+    this.row = row;
+    this.column = column;
+  }
+
+  public int getRow(){
+    return row;
+  }
+  public int getColumn(){
+    return column;
+  }
+
+  private static void checkCoordinates(int row, int column) {
+    if (row < 1 || row > 8) {
+      throw new IllegalArgumentException("Некорректный номер строки: " + row);
+    }
+
+    if (column < 1 || column > 8) {
+      System.out.println("Некорректный номер столбца: " + column);
+      throw new IllegalArgumentException();
+    }
+  }
 }
